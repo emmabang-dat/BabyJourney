46d8daa64c268da0520df336f242ecd9a6f07bc9
Arnsdorf <38246817+Arnsdorf@users.noreply.github.com>
2023-10-23 12:54:25 +0200

    Modal opstart

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { MyModalComponent } from '../my-modal/my-modal.component';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  constructor(private modalController: ModalController) {}
 
-  constructor() {}
-
+  async openModal() {
+    const modal = await this.modalController.create({
+      component: MyModalComponent,
+      componentProps: {
+        // Her kan du tilføje eventuelle data, som du vil sende til modalen
+      },
+    });
+    return await modal.present();
+  }
 }
