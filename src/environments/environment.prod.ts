dad4f842afbfd3076654752f28054ccbf13db225
Emma Bang <83577644+emmabang-dat@users.noreply.github.com>
2023-10-23 12:57:19 +0200

    Firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'babyjourney-ab9bb',
+    appId: '1:222476118206:web:0a8b8001dbc15c9d7796ef',
+    storageBucket: 'babyjourney-ab9bb.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDw_9O3FpPpiY7D9AYLNGlLBFEG66hpPhw',
+    authDomain: 'babyjourney-ab9bb.firebaseapp.com',
+    messagingSenderId: '222476118206',
+  },
   production: true
 };
