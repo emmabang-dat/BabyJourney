016701f3b582a1c5e97912d02308a1e8c0bc70cc
MikkelLebedev <mikkel.lebedev@outlook.com>
2023-11-03 11:12:06 +0100

    lidt css styling

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private router: Router) { }
+    openTimeline(){
+      this.router.navigate(['/timeline'])
+    }
+    
+    
+    
   ngOnInit() {
   }
-
 }
+
+
